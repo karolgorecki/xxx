665953b262c3d72f65a84d62e4ece62252c76431
Karol Gorecki <hi@karolgorecki.pl>
2017-01-29 18:06:25 +0100

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,9 +1,11 @@
 ﻿{
-	"version": 1485378274,
+	"version": 1485709203,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
